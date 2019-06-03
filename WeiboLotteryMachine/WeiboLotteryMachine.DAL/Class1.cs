eaa277f65596d5b5a891545fa054bd7c074ee7eb
袁宏来 <yuanhl@tuya.com>
2019-06-03 17:15:57 +0800

    创建工程

---
+++ WeiboLotteryMachine/WeiboLotteryMachine.DAL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeiboLotteryMachine.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ WeiboLotteryMachine/WeiboLotteryMachine.Model/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeiboLotteryMachine.Model
+{
+    public class Class1
+    {
+    }
+}
