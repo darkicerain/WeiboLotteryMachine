836232c02e293c7243e55c73770aee5c9ca64480
袁老三 <yhonglai@163.com>
2019-12-15 22:20:00 +0800

    添加WPF工程

---
+++ WeiboLotteryMachine/WeiboLotteryMachine.WPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+using GalaSoft.MvvmLight;
+
+namespace WeiboLotteryMachine.WPF.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+
+        }
+    }
+}
